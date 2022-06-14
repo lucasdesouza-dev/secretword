87e9361a30a0c0e1de2709104839c734a9ed1cef
lusousouza <luca.s.ouza@hotmail.com>
2022-06-14 15:12:01 -0300

    melhorias no card

--- src/data/Words.js
+++ src/data/Words.js
@@ -1,9 +1,9 @@
 export const wordsList = {
-  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena","Volante","Banco"],
-  fruta: ["Banana", "Maça", "Pêra", "Mamão", "Laranja","Uva","Melancia"],
-  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos","Peito","Mao"],
-  computador: ["Mouse", "Teclado", "Monitor", "Gabinete","Cooler","Phone"],
-  programação: ["Linguagem", "Framework", "JavaScript", "React","HTML","Bootstrap"],
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Banco"],
+  fruta: ["Banana", "Maça", "Pêra", "Mamão", "Laranja", "Uva", "Melancia"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Peito", "Mao"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Cooler", "Phone"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "HTML", "Bootstrap"],
   alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
-  nomes: ["Lucas","Stefany","Pyetra","Luana","Sophia","Florenca"]
-};
+  nomes: ["Lucas", "Stefany", "Pyetra", "Luana", "Sophia", "Florenca"]
+}; 
